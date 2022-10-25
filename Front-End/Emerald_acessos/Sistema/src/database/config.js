cf3e5f8f98a5a9ceaf6a480a355ec6a447790db2
Paulo Cezaretto <paulo.alvares@sptech.school>
2022-10-25 14:44:31 -0300

    Adicionando credenciais do MySql na API

--- Front-End/Emerald_acessos/Sistema/src/database/config.js
+++ Front-End/Emerald_acessos/Sistema/src/database/config.js
@@ -20,9 +20,9 @@ var sqlServerConfig = {
 // CONEXÃO DO MYSQL WORKBENCH (LOCAL)
 var mySqlConfig = {
     host: "localhost",
-    database: "SEU_DATABASE",
-    user: "SEU_USUARIO",
-    password: "SUA_SENHA",
+    database: "sprint02",
+    user: "root",
+    password: "sptech",
 };
 
 function executar(instrucao) {
